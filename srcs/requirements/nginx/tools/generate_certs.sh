c6c9d338cfc746a641424f3d6087f6c214971ebc
Konrad Sudyn <ksudyn@c1r11s1.42madrid.com>
2026-01-16 20:35:36 +0100

    Inicio de explicacion de NIGNX

--- srcs/requirements/nginx/tools/generate_certs.sh
+++ srcs/requirements/nginx/tools/generate_certs.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+# Esto le dice al sistema que use Bash, que esta disponible para Debian.
 set -e
+# Si ocurre algun error se detiene el script.
 
-# Ruta donde se guardarán los certificados
+
+# CERT_PATH es una variable donde se guarda la ruta donde se guardarán los certificados
+# Se pone esa ruta porque Docker monta secrest automaticamente ahí
 CERT_PATH=/run/secrets
 
-# Dominio, por defecto localhost
+# DOMAIN (dominio) es el nombre de tu página web
+# En Inception seria login.42.fr
+# Si existe DOMAIN_NAME, se usa, si no, se usa loclahost.
 DOMAIN=${DOMAIN_NAME:-localhost}
+
+# ! -f        -> significa si no existe este archivo ( -f es archivo y ! es no)
+# El resto se refiere a si existe el certificado y su contraseña
+# Ese certificado sirve para permitir que tu web funcione con HTTPS (https://)
+# y cifrar la comunicación entre el navegador y tu servidor NGINX
+# Sin ellos HTTPS no puede arrancar el puerto 443 y NGINX no puede arrancar el puerto 443
+
+# openssl req           -> Crea un certificado SSL
+# -x509                 -> Indica que es un certificado autofirmado
+# -nodes                -> Significa no cifres la clave privada,
+#                           significa que no hace falta dar ninguna contraseña,
+#                           si no, te pide una clave que no puedes dar y da error
+# -days 365             -> significa que valida esto durante 365 días
+
 
 # Solo genera certificados si no existen
 if [ ! -f "$CERT_PATH/nginx.crt" ] || [ ! -f "$CERT_PATH/nginx.key" ]; then
